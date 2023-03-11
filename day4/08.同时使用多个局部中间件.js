f367dbe459b4549db2262f926b4cf568f6afc93a
tom422 <71369779+tom422@users.noreply.github.com>
2023-03-11 12:09:03 +0000

    中间件的学习

---
+++ day4/08.同时使用多个局部中间件.js
@@ -0,0 +1,30 @@
+// 1. 导入 express 模块
+const express = require('express')
+// 2. 创建 express 的服务器实例
+const app = express()
+
+// 1. 定义中间件函数
+const mw1 = (req,res,next)=>{
+    console.log('调用了第一个局部生效的中间件');
+    next()
+}
+
+const mw2 = (req,res,next)=>{
+    console.log('调用了第二个局部生效的中间件');
+    next()
+}
+
+// 2. 创建路由
+app.get('/', [mw1, mw2], (req,res)=>{
+    res.send('Hemo page.')
+})
+
+app.get('/user',(req,res)=>{
+    res.send('User page.')
+})
+
+
+// 调用 app.listen 方法，指定端口号并启动web服务器
+app.listen(8080,()=>{
+    console.log("server running at http://127.0.0.1:8080"); 
+})
---
+++ day4/09.演示错误级别中间件的使用.js
@@ -0,0 +1,23 @@
+// 1. 导入 express 模块
+const express = require('express')
+// 2. 创建 express 的服务器实例
+const app = express()
+ 
+
+// 2. 创建路由
+app.get('/', (req,res)=>{
+    // 1.1 人为制造错误
+    throw new Error('服务器内部发生了错误！')
+    res.send('Hemo page.')
+})
+
+// 2. 定义错误级别的中间件，捕获整个项目的异常错误，从而防止程序的崩溃
+app.use((err,req,res,next)=>{
+    console.log('发生了错误！' + err.message);
+    res.send('Error:' + err.message)
+})
+ 
+// 调用 app.listen 方法，指定端口号并启动web服务器
+app.listen(8080,()=>{
+    console.log("server running at http://127.0.0.1:8080"); 
+})
