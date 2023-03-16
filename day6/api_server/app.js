d79a55a5f5660e9afb07f356216766c7b34dc37d
qinxuanlong <1470681411@qq.com>
2023-03-16 21:28:07 +0800

    注册模块

--- day6/api_server/app.js
+++ day6/api_server/app.js
@@ -10,10 +10,36 @@ app.use(cors())
 //  配置解析表单数据的中间件 ，注意：这个中间件，只能解析 application/x-www-form-urlencoded 格式的表单数据
 app.use(express.urlencoded({ extended: false }))
 
+// 一定要在路由之前，封装 res.cc 函数
+app.use((req, res, next) => {
+  // status = 0 为成功； status = 1 为失败； 默认将 status 的值设置为 1，方便处理失败的情况
+  res.cc = function (err, status = 1) {
+    res.send({
+      // 状态
+      status,
+      // 状态描述，判断 err 是 错误对象 还是 字符串
+      message: err instanceof Error ? err.message : err,
+    })
+  }
+  next()
+})
+
 // write your code here...
 // 导入并注册用户路由模块
 const userRouter = require('./router/user')
 app.use('/api', userRouter)
+
+const joi = require('joi')
+// 定义错误级别的中间件
+app.use((err,req,res,next)=>{
+
+  // 验证失败导致的错误
+    if (err instanceof joi.ValidationError) return res.cc(err)
+
+    // 未知的错误
+    res.cc(err)
+     
+})
 
 
 // 调用 app.listen 方法，指定端口号并启动web服务器
--- day6/api_server/router_handler/user.js
+++ day6/api_server/router_handler/user.js
@@ -1,28 +1,53 @@
+// 导入数据库操作模块
 const db = require('../db/index')
-
+// 导入 bcryptjs 这个包
+const bcryptjs = require('bcryptjs')
 // 注册新用户的处理函数
 exports.regUser = (req, res) => {
     const userInfo = req.body;
-    console.log(userInfo);
+    // console.log(userInfo);
     // 判断数据是否合法
     if (!userInfo.username || !userInfo.password) {
-        return res.send({ status: 1, message: '用户名或密码不能为空！' })
+        return res.cc('用户名或密码不能为空！') 
+        // res.send({ status: 1, message: '用户名或密码不能为空！' })
     }
 
     // 定义 SQL 语句， 查询用户名是否被占用
     const sqlStr = 'select * from ev_users where username = ? '
     db.query(sqlStr,userInfo.username,(err,results)=>{
         // 执行 SQL 语句失败
-        if (err) {
-            return res.send({ status:1, message:err.message })
-        }
+        // if (err) {
+        //     // return res.send({ status:1, message:err.message })
+        //     return res.cc(err)
+        // }
 
         // 判断用户名是否被占用
         if (results.length > 0) {
-            return res.send({ status:1, message:'用户名被占用，请更换其他用户名！'})
+            // return res.send({ status:1, message:'用户名被占用，请更换其他用户名！'})
+            return res.cc('用户名被占用，请更换其他用户名！')
         }
         
-        // TODO: 用户名可用，继续后续流程...
+        // 调用 bcrypt.hashSync() 对密码进行加密
+        // console.log(userInfo);
+        userInfo.password = bcryptjs.hashSync(userInfo.password,10)
+        // console.log(userInfo);
+        
+        // 定义插入新用户的 SQL 语句
+        const sql = 'insert into ev_users set ?'
+        // 调用 db.query() 执行 sql 语句
+        db.query(sql, { username: userInfo.username, password: userInfo.password},(err, results) => {
+            // 执行 SQL 语句失败
+            // if (err) return res.send({ status: 1, message: err.message })
+            if (err) return res.cc(err)
+            // SQL 语句执行成功，但影响行数不为 1
+            if (results.affectedRows !== 1) {
+            //   return res.send({ status: 1, message: '注册用户失败，请稍后再试！' })
+                return res.cc('注册用户失败，请稍后再试！')
+            }
+            // 注册成功
+            // res.send({ status: 0, message: '注册成功！' })
+            return res.cc('注册成功！', 0)
+          })
     })
 
     // res.send('reguser OK')
